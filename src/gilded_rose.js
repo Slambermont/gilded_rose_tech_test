1c3bbbf31c22828989d4a3f6b17676a1a65710e2
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-21 10:42:30 +0100

    Refactor updateQuality into different function for each type of item

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,35 +14,46 @@ class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name === "Aged Brie") {
-        this.decreaseSellIn(i)
-        this.increaseQuality(i)
-        if (this.items[i].sellIn < 0) {
-          this.increaseQuality(i)
-        }
+        this.updateQualityBrie(i)
       } else if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
-
       } else if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
-        this.decreaseSellIn(i)
-        if (this.items[i].sellIn < 0) {
-          this.items[i].quality = 0
-        } else {
-          this.increaseQuality(i)
-          if (this.items[i].sellIn < 10) {
-            this.increaseQuality(i)
-            if (this.items[i].sellIn < 5) {
-              this.increaseQuality(i)
-            }
-          }
-        }
+        this.updateQualityBackstage(i)
       } else {
-        this.decreaseSellIn(i)
-        this.decreaseQuality(i)
-        if (this.items[i].sellIn < 0) {
-          this.decreaseQuality(i)
+        this.updateQualityOther(i)
+      }
+      return this.items
+    }
+  }
+
+  updateQualityBrie(i) {
+    this.decreaseSellIn(i)
+    this.increaseQuality(i)
+    if (this.items[i].sellIn < 0) {
+      this.increaseQuality(i)
+    }
+  }
+
+  updateQualityBackstage(i) {
+    this.decreaseSellIn(i)
+    if (this.items[i].sellIn < 0) {
+      this.items[i].quality = 0
+    } else {
+      this.increaseQuality(i)
+      if (this.items[i].sellIn < 10) {
+        this.increaseQuality(i)
+        if (this.items[i].sellIn < 5) {
+          this.increaseQuality(i)
         }
       }
     }
-    return this.items;
+  }
+
+  updateQualityOther(i) {
+    this.decreaseSellIn(i)
+    this.decreaseQuality(i)
+    if (this.items[i].sellIn < 0) {
+      this.decreaseQuality(i)
+    }
   }
 
   decreaseSellIn(index) {
